4b8687f84598e81f61e5b0c778a8a70a8fa2f2e2
Deploy from CI <>
2019-11-28 17:23:54 +0000

    Deploy d733ebf3abd5fc074e93fd283f0a894e986ecbcb to gh-pages

---
+++ api/implementors/alloc/string/trait.ToString.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["wasmtime"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/alloc/string/trait.ToString.html\" title=\"trait alloc::string::ToString\">ToString</a> for <a class=\"struct\" href=\"wasmtime/struct.Name.html\" title=\"struct wasmtime::Name\">Name</a>",synthetic:false,types:["wasmtime::types::Name"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
